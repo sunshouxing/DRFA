612b3711f4675da9f0320a6e4d4d66d9c97b3a8f
Arthur <shouxing.sun@aliyun.com>
2017-06-07 23:41:12 +0800

    implement django REST api

--- server/config/urls.py
+++ server/config/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from rest_framework.routers import DefaultRouter
+
+from retail.views import ChainViewSet, StoreViewSet, EmployeeViewSet
+
+router = DefaultRouter()
+router.register(prefix='chains', viewset=ChainViewSet)
+router.register(prefix='stores', viewset=StoreViewSet)
+router.register(prefix='employees', viewset=EmployeeViewSet)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
+
+urlpatterns += router.urls
--- server/retail/views.py
+++ server/retail/views.py
@@ -1,6 +1,28 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 
-from django.shortcuts import render
+from __future__ import unicode_literals
+from rest_framework import viewsets
+
+from .models import Chain, Store, Employee
+from .serializers import ChainSerializer, StoreSerializer, EmployeeSerializer
+
 
 # Create your views here.
+class ChainViewSet(viewsets.ModelViewSet):
+    """ ViewSet for viewing and editing Chain objects """
+    queryset = Chain.objects.all()
+    serializer_class = ChainSerializer
+
+
+class StoreViewSet(viewsets.ModelViewSet):
+    """ ViewSet for viewing and editing Store objects """
+    queryset = Store.objects.all()
+    serializer_class = StoreSerializer
+
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    """ ViewSet for viewing and editing Employee objects """
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+# EOF
